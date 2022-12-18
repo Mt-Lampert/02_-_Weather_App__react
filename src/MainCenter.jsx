e8838d6784b8a3de699171c34d9b0d9731fced0b
Mt-Lampert <perrin.blog@web.de>
2022-12-18 10:08:55 +0100

    w: implementing the 'MainCenter' component

--- src/MainCenter.jsx
+++ src/MainCenter.jsx
@@ -1,10 +1,50 @@
+function getSVGfor(desc) {
+  switch (desc) {
+    case "cloudy":
+      return "cloudy.svg";
+      break;
+    case "few clouds":
+      return "sunny.svg";
+      break;
+    case "light snow":
+      return "light_snow.svg";
+      break;
+    case "overcast":
+      return "overcast.svg";
+      break;
+    case "rain":
+      return "rain.svg";
+      break;
+    case "sunny":
+      return "sunny.svg";
+      break;
+
+    default:
+      return "EH?";
+      break;
+  }
+}
 
 function MainCenter(props) {
-  return ( 
+  // Outline:
+  // we need three props:
+  //   - the name of the city
+  //   - the weather condition
+  //   - the temperature
+  //
+  // the weather condition has to be transformed into a svg file name.
+  // this is the job of a helper function with a switch{}
+  //
+
+  return (
     <div>
-      <h1>This comes after success!</h1>
+      <figure>
+        <img src={"./assets/" +getSVGfor(props.sky)} alt={props.sky + " weather Symbol"} />
+      </figure>
+      <p>{props.city}</p>
+      <p>{props.temperature}</p>
     </div>
-   );
+  );
 }
 
 export default MainCenter;
---
+++ src/MainCenter.spec.jsx
@@ -0,0 +1,24 @@
+import { getByAltText, render, screen } from "@testing-library/react";
+import MainCenter from "./MainCenter";
+
+describe("MainCenter", () => {
+  beforeEach(() => {
+    render(<MainCenter city="London, UK" sky="sunny" temperature="22,3" />);
+
+  })
+  it("has a weather image", () => {
+    const image = screen.getByAltText(/sunny/i);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute("src", expect.stringContaining("sunny.svg"));
+  });
+
+  it("has a city label", () => {
+    const nameTag = screen.getByText("London, UK");
+    expect(nameTag).toBeInTheDocument();
+  });
+
+  it("has a temperature label", () => {
+    const tempTag = screen.getByText("22,3");
+    expect(tempTag).toBeInTheDocument();
+  });
+});

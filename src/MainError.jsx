7e3517a8a39c31d646534d91a8cad3f9241f754d
Mt-Lampert <perrin.blog@web.de>
2022-12-19 20:04:11 +0100

    w: formatting the error message

--- src/MainError.jsx
+++ src/MainError.jsx
@@ -1,10 +1,14 @@
+import "./MainError.scss";
+
 function MainError(props) {
-  return ( 
-    <div className="main-error container is-centered">
-      <h1>This is an error!</h1>
-      <p>{props.message}</p>
+  return (
+    <div className="main-error">
+      <div className="my-message notification is-danger">
+        <h1>This is an error!</h1>
+        <p>{props.message}</p>
+      </div>
     </div>
-   );
+  );
 }
 
 export default MainError;
